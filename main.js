68cb05229196dd7ab42be1f462b054519748d18a
CL <carolinemv.le@gmail.com>
2021-10-22 11:37:48 -0700

    creates DOM elements and CSS

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('main.js connected')
+
+const feedButton = document.querySelector('#feed');
+const playButton = document.querySelector('#play');
+const napButton = document.querySelector('#nap');
+
