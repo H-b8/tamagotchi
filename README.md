756d380490cbc9666d5f3dd3a7657b5c7f0f22fe
CL <carolinemv.le@gmail.com>
2021-10-27 14:33:27 -0700

    adds depression to game logic

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 - Your pet will reject play if they have no energy
 - Feeding your pet will fill their tummies and make them happy
 - Overfeeding your pet will cause them to lose energy and reject food
-- Putting your pet to bed restores energy and increases hunger. But like every living creature, your pet sometimes wakes up in a better or worse mood
+- Putting your pet to bed restores energy and increases hunger. But like every living creature, your pet can wake up in a better or worse mood
 - When happines at 0%, your pet will become depressed and only want to sleep
 - In a depressed state, your only hope is that your pet might wake up in a good mood and decide to accept food or play
 
--- js/objOriented.js
+++ js/objOriented.js
@@ -25,101 +25,129 @@ class Pet {
         this.energy = Math.floor(Math.random() * 100);
     };
 
+    resetButtons() {
+        playText.innerText = 'PLAY';
+        playButton.style.backgroundColor = 'rgb(140, 123, 252)';
+        feedText.innerText = 'FEED';
+        feedButton.style.backgroundColor = 'rgb(140, 123, 252)';
+    }
+
+    rejectPlay() {
+        playText.innerText = '';
+        playButton.style.backgroundColor = 'rgb(140, 123, 252, 0.5)';
+    }
+
+    rejectFood() {
+        feedText.innerText = '';
+        feedButton.style.backgroundColor = 'rgb(140, 123, 252, 0.5)';
+    }
+
     updateStats() {
-        if (this.tummy <= 0 || this.energy <= 0) { // if exhausted or starving, lower mood and reject play
-            this.happiness -= 20;
-            playText.innerText = '';
-            playButton.style.backgroundColor = 'rgb(140, 123, 252, 0.5)';
-        } else {
-            playText.innerText = 'PLAY';
-            playButton.style.backgroundColor = 'rgb(140, 123, 252)';
+        this.resetButtons();
+        
+        if (this.tummy <= 0) { // if starved
+            console.log('im getting hangry and dont want to play anymore')
+            this.happiness -= ((-this.tummy) * 2); // decrease happiness
+            this.energy -= ((-this.tummy) * 3); // decrease energy
+            this.rejectPlay();
+        } else if (this.tummy >= 100) { // if too full
+            console.log('im too full!')
+            this.energy -= ((this.tummy - 100) * 3); // decrease energy
+            this.rejectFood();
         }
 
-        if (this.tummy >= 100) { // if tummy is too full, reject food
-            feedText.innerText = '';
-            feedButton.style.backgroundColor = 'rgb(140, 123, 252, 0.5)';
-        } else {
-            feedText.innerText = 'FEED';
-            feedButton.style.backgroundColor = 'rgb(140, 123, 252)';
+        if (this.happiness <= 0) { // if depressed
+            console.log('im too sad to play or eat right now...')
+            this.rejectPlay();
+            this.rejectFood();
         }
 
-        if (this.happiness < 0) this.happiness = 0;
+        if (this.energy <= 0) { // if no energy
+            console.log('im too tired to play right now...')
+            this.rejectPlay();
+        }
+        
         if (this.tummy < 0) this.tummy = 0;
+        
+        if (this.happiness < 0) this.happiness = 0;
+        else if (this.happiness > 100) this.happiness = 100;
+        
         if (this.energy < 0) this.energy = 0;
+        else if (this.energy > 100) this.energy = 100;
+        
 
         happyStat.innerText = this.happiness;
         tummyStat.innerText = this.tummy;
         energyStat.innerText = this.energy;
     };
 
+    startAction() {
+        buttonContainer.classList.add('disable-buttons');
+        petContainer.classList.remove('pacing');
+        petContainer.classList.add('center-pet');
+    }
+
+    endAction() {
+        buttonContainer.classList.remove('disable-buttons');
+        petContainer.classList.add('pacing');
+        petContainer.classList.remove('center-pet');
+    }
+
     play() {
         if (this.energy > 0) {
-            buttonContainer.classList.add('disable-buttons');
-            petContainer.classList.remove('pacing');
-            petContainer.classList.add('center-pet');
+            console.log('weeeeeeee!');
+
+            this.startAction();
             petGraphic.classList.add('playing');
 
-            this.happiness += Math.floor(Math.random() * 50);
-            this.energy -= 10;
-            this.tummy -= 20;
+            this.happiness += Math.floor(Math.random() * 50); // increase happiness by varying amount
+            this.energy -= 20;
+            this.tummy -= 25;
 
             setTimeout(() => {
-                petContainer.classList.remove('center-pet')
                 petGraphic.classList.remove('playing');
-                petContainer.classList.add('pacing');
-                buttonContainer.classList.remove('disable-buttons');
-            }, 7000);
-
-            this.updateStats();
+                this.endAction();
+                this.updateStats();
+            }, 5000);
         };
     };
 
     feed() {
         if (this.tummy < 100) {
-            buttonContainer.classList.add('disable-buttons');
-            petContainer.classList.remove('pacing');
-            petContainer.classList.add('center-pet');
+            console.log('thank u');
+
+            this.startAction();
             petGraphic.classList.add('eating');
 
+            this.happiness += Math.floor(Math.random() * 10); // increases happiness by varying amount
+            this.energy += 15;
             this.tummy += 30;
-            this.energy += 10;
-            this.happiness += Math.floor(Math.random() * 10);
-
-            if (this.tummy > 100) {
-                console.log('you overfed me!');
-                this.energy -= 60;
-                this.happiness -= 30;
-            };
 
             setTimeout(() => {
-                petContainer.classList.remove('center-pet')
                 petGraphic.classList.remove('eating');
-                petContainer.classList.add('pacing');
-                buttonContainer.classList.remove('disable-buttons');
-            }, 5000);
-
-            this.updateStats();
+                this.endAction();
+                this.updateStats();
+            }, 3000);
         };
     };
 
     sleep() {
-        buttonContainer.classList.add('disable-buttons');
-        petContainer.classList.remove('pacing');
-        petContainer.classList.add('center-pet');
+        console.log('goodnight');
+
+        this.startAction();
         petGraphic.classList.add('sleeping');
 
-        this.energy += Math.floor(Math.random() * 40); // varying level of energy increase
-        this.tummy -= Math.floor(Math.random() * 20); // varying level of hunger increase (decrease tummy filled)
-        this.happiness += Math.floor(Math.random() * (-5 - 50)); // possibility of waking up in a better or worse mood
+        let mood = Math.floor(Math.random() * 50) * (Math.round(Math.random()) ? 1 : -1);
+
+        this.happiness += mood; // possibility of waking up in a better or worse mood
+        this.energy += Math.floor(Math.random() * 30); // increase energy by varying amount
+        this.tummy -= Math.floor(Math.random() * 20); // increase hunger
 
         setTimeout(() => {
-            petContainer.classList.remove('center-pet');
             petGraphic.classList.remove('sleeping');
-            petContainer.classList.add('pacing');
-            buttonContainer.classList.remove('disable-buttons');
-        }, 9000);
-
-        this.updateStats();
+            this.endAction();
+            this.updateStats();
+        }, 7000);
     };
 };
 
